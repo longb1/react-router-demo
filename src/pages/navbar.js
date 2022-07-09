fcdb17fd5fbb16ef9ce67ea96303abc47610e6ce
Long <17272827+longb1@users.noreply.github.com>
2022-07-09 12:56:01 +0100

    use router Link component
    use router link component to enable navigation via clicking nav bar
    items.

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,18 +1,24 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export default function NavBar() {
     return (
         <nav>
             <ul>
+            <Link to="/">
                 <li>
                     Home
                 </li>
+            </Link>
+            <Link to="/pages/about">
                 <li>
                     About
                 </li>
+            </Link>
+            <Link to="/pages/contact">
                 <li>
                     Contact
                 </li>
+            </Link>
             </ul>
         </nav>
         
