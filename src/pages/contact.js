c14139106142017845c1c022b8e88d67c43b283c
Long <17272827+longb1@users.noreply.github.com>
2022-07-09 12:38:39 +0100

    initial commit

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div id="contact">
+        <h1>this is about page</h1>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </nav>
+        
+    );
+}
